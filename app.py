5f48572ce02f9f5102d673708f70f5a33465bba7
Anil <anilarelli5@gmail.com>
2026-03-25 16:42:00 +0530

    docker compose

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-from flask import Flask, request
+from flask import Flask
+import redis
 import os
 
 app = Flask(__name__)
 
-# VULNERABILITY 1: hardcoded secret
-app.config["SECRET_KEY"] = "super-hardcoded-secret"
+redis_host = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=redis_host, port=6379)
 
-# VULNERABILITY 2: weak input handling
 @app.route("/")
 def home():
-    name = request.args.get("name", "guest")
-    return f"Hello {name}"
-
-# VULNERABILITY 3: debug info style route
-@app.route("/config")
-def config():
-    return {
-        "debug": True,
-        "secret_key": app.config["SECRET_KEY"],
-        "env_message": os.getenv("APP_MESSAGE", "not-set")
-    }
+    count = r.incr("hits")
+    return f"Hello DevSecOps 🚀 - Visits: {count}"
 
 if __name__ == "__main__":
-    # VULNERABILITY 4: debug mode enabled
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
